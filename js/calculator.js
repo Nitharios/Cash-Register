12864b93d748298a18983467a66f7a27c3bcfc84
Nathan N <Nitharios@users.noreply.github.com>
2017-08-28 10:09:38 -1000

    Copied code from js-calculator assignment to calculator.js

---
+++ js/calculator.js
@@ -0,0 +1,111 @@
+/**
+ * Declare a function named `calculatorModule`
+ * this function will have two private variables declared inside of it.
+ * @variable PRIVATE { Number } `memory`
+ * @variable PRIVATE { Number } `total`
+ * @return {object} `calculator` object that can be used
+ */
+ var calculatorModule = (function() {
+
+    var memory = 0, total = 0;
+
+    return calculator = {
+      load: setTotal,
+      getTotal: getTotal,
+      add: add,
+      subtract: subtract,
+      multiply: multiply,
+      divide: divide,
+      recallMemory: recallMemory,
+      saveMemory: saveMemory,
+      clearMemory: clearMemory
+    }
+
+
+  /**
+   * sets the `total` to the number passed in
+   * @param  { Number } x
+   * @return { Number }    current total
+   */
+    function setTotal(x) {
+      validate(x)
+      total = x
+      return total
+   }
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+    function getTotal() {
+      return total
+   }
+
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+    function add(x) {
+      validate(x)
+      total += x
+    }
+
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+    function subtract(x) {
+      validate(x)
+      total -= x
+    }
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+    function multiply(x) {
+      validate(x)
+      total *= x
+    } 
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+    function divide(x) {
+      validate(x)
+      total /= x
+    }
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+    function recallMemory() {
+      return memory
+    }
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+    function saveMemory() {
+      memory = total
+    }
+
+  /**
+   * Clear the value stored at `memory`
+   */
+    function clearMemory() {
+      memory = 0
+    }
+
+  /**
+   * Validation
+   */
+    function validate(x) {
+      if (typeof x !== 'number') {
+        throw new Error('Invalid')
+      }
+    }
+})
