aca3cad7a2a7bf64f32cc6824e8e85fa4796ce33
Nathan N <Nitharios@users.noreply.github.com>
2017-08-29 19:53:39 -1000

    Cleaned up functions, removed add/sub/mul/div functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,48 +1,50 @@
 
 var calculator = (function() {
 
-  total = 0;
+  var total = 0;
+
+  function getTotal() {
+    return total;
+  }
+
+  function resetTotal() {
+    total = 0;
+    return total;
+  }
+
+  function addToTotal(num) {
+    total += num;
+  }
+
+  function subtractFromTotal(num) {
+    total -= num;
+  }
+
+  function add(x, y) {
+    return x + y;
+  }
+
+  function subtract(x, y) {
+    return x - y;
+  }
+
+  function multiply(x, y) {
+    return x * y;
+  } 
+
+  function divide(x, y) {
+    return x / y;
+  }
 
   return calculator = {
     getTotal: getTotal,
-    resetTotal: resetTotal,
+    reset: resetTotal,
+    deposit: addToTotal,
+    withdraw: subtractFromTotal,
     add: add,
     subtract: subtract,
     multiply: multiply,
     divide: divide
   }
 
-  function getTotal() {
-    return total
-  }
-
-  function resetTotal() {
-    total = 0
-  }
-
-  function add(x) {
-    // validate(x)
-    total += x
-  }
-
-  function subtract(x) {
-    validate(x)
-    total -= x
-  }
-
-  function multiply(x) {
-    validate(x)
-    total *= x
-  } 
-
-  function divide(x) {
-    validate(x)
-    total /= x
-  }
-
-  function validate(x) {
-    if (typeof x !== 'number') {
-      throw new Error('Invalid')
-    }
-  }
-})
+})()
--- js/cash_register.js
+++ js/cash_register.js
@@ -1,87 +1,121 @@
-var calculator = new calculator()
 
-// Retrieves key input from index
-var keys = document.querySelectorAll('#cashRegister span')
-var operationArr = ['+', '-', 'x', '÷']
-var result
+// Revealing Module Pattern to prevent function variables from being affected outside the function's scope
+var registerLogic = (function () {
+  // Creates a nodeList of all the keys under #cashRegister
+  var digits = document.querySelectorAll('.digit');
+  var specialKeys = document.querySelectorAll('.special');
+  var operations = document.querySelectorAll('operation');
+  var options = document.querySelectorAll('.option');
+  var decimalAdded = false;
+  var zeroZeroAdded = false;
 
-// Create onclick event for keys
-for (var i = 0; i < keys.length; i++) {
-  keys[i].onclick = function(e) {
-    // Retrieve input and key values
-    var input = document.querySelector('.userInput')
-    var inputVal = input.innerHTML     // current input on screen before key-stroke
-    var keyVal = this.innerHTML        // key-stroke
-
-    if (keyVal === '=') {
-      var tempResult = 0
-      var equation = inputVal.split(' ')
-
-      while (equation.indexOf('x') !== -1 || equation.indexOf('÷') !== -1) {
-
-        var multiplyIdx = equation.indexOf('x')
-        var divideIdx = equation.indexOf('÷')
-
-        if (multiplyIdx < divideIdx && multiplyIdx > 0 || divideIdx === -1) {
-          tempResult = equation[multiplyIdx-1] * equation[multiplyIdx+1]
-          equation.splice(multiplyIdx-1, 3, tempResult)
-
-        } else if (divideIdx < multiplyIdx && divideIdx > 0 || multiplyIdx === -1) {
-          tempResult = equation[divideIdx-1] / equation[divideIdx+1]
-          equation.splice(divideIdx-1, 3, tempResult)
-        }
-      }
-
-      while (equation.indexOf('+') !== -1 || equation.indexOf('-') !== -1) {
-
-        var addIdx = equation.indexOf('+')
-        var subIdx = equation.indexOf('-')
-
-        if (addIdx < subIdx && addIdx > 0 || subIdx === -1) {
-          tempResult = parseFloat(equation[addIdx-1]) + parseFloat(equation[addIdx+1])
-          equation.splice(addIdx-1, 3, tempResult)
-
-        } else if (subIdx < addIdx && subIdx > 0 || addIdx === -1) {
-          tempResult = equation[subIdx-1] - equation[subIdx+1]
-          equation.splice(subIdx-1, 3, tempResult)
-        }
-      }
-
-      input.innerHTML = tempResult
-
-    } else if (keyVal === 'reset') {
-      calculator.resetTotal()
-      input.innerHTML = '0'
-
-    } else if (keyVal === 'clear') {
-      input.innerHTML = ''
-
-    } else if (keyVal === 'balance') {
-      input.innerHTML = calculator.getTotal()
-
-    } else if (keyVal === 'deposit') {
-      calculator.add(parseFloat(inputVal))
-      input.innerHTML = ''
-
-    } else if (keyVal === 'withdraw') {
-      calculator.subtract(parseFloat(inputVal))
-      input.innerHTMl = ''
-
-    } else {
-
-      if (inputVal.indexOf(0) === 0 && operationArr.indexOf(keyVal) === -1 && keyVal !== 0) {
-        input.innerHTML = keyVal
-
-      } else if (inputVal.indexOf(0) === 0 && keyVal === 0) {
-        input.innerHTML = 0
-      
-      } else if (keyVal.search('[0-9]+') !== -1) {
-        input.innerHTML += keyVal
-
-      } else {
-        input.innerHTML += ' ' + keyVal + ' '
-
-      }
-    }
+  // Loop to assign event function for each digit
+  for (var i = 0; i < digits.length; i++) {
+    digits[i].onclick = registerDigit;
   }
-}
+
+  // Loop to assign event function for each special key
+  for (var i = 0; i < specialKeys.length; i++) {
+    specialKeys[i].onclick = registerSpecial;
+  }
+
+  // Loop to assign event function for each operation
+  for (var i = 0; i < operations.length; i++) {
+    operations[i].onclick = registerOperation;
+  }
+
+  // Loop to assin event function for each option
+  for (var i = 0; i < options.length; i++) {
+    options[i].onclick = registerOption;
+  }
+
+  function registerDigit(event) {
+    var input = document.querySelector('.userInput');
+    var keyChoice = this.innerHTML;
+
+    // Inputs the key in the userInput section of the UI
+    if (input.innerHTML.indexOf(0) === 0) {
+      input.innerHTML = keyChoice;
+    
+    } else {
+      input.innerHTML += keyChoice;
+
+    }
+  // end of registerDigit function  
+  }
+
+  function registerSpecial(event) {
+    var input = document.querySelector('.userInput');
+    var keyChoice = this.innerHTML;
+
+    // changes keyChoice to true for some reason instead of expected . or 00
+    if (keyChoice = '.' && decimalAdded === false) {
+      decimalAdded = true;
+      input.innerHTML += '.';
+
+    } else if (keyChoice = '00' && zeroZeroAdded === false) {
+      zeroZeroAdded = true;
+      input.innerHTML += '00';
+    }
+  // end of registerSpecial function
+  }
+
+  function registerOperation(event) {
+    var input = document.querySelector('.userInput');
+    var keyChoice = this.innerHTML;
+
+    if (keyChoice === '=') {
+
+
+    } else if (keyChoice === '+') {
+
+
+    } else if (keyChoice === '-') {
+
+
+    } else if (keyChoice === 'x') {
+
+
+    } else if (keyChoice === '÷') {
+
+
+    }
+  // end of registerOperation function
+  }
+
+  // Function called onto each key
+  function registerOption(event) {
+    // Registers the current input BEFORE last key press
+    var input = document.querySelector('.userInput');
+    // Registers most recent key press
+    var keyChoice = this.innerHTML;
+
+    if (keyChoice === 'reset') {
+      calculator.reset();
+      clear();
+
+    } else if (keyChoice === 'clear') {
+      clear();
+
+    } else if (keyChoice === 'balance') {
+      input.innerHTML = calculator.getTotal();
+    
+    } else if (keyChoice === 'deposit') {
+      calculator.deposit(parseFloat(input.innerHTML));
+      clear();
+
+    } else if (keyChoice === 'withdraw') {
+      calculator.withdraw(parseFloat(input.innerHTML));
+      clear();
+    }
+
+  // end of registerKey function 
+  }
+
+  function clear() {
+    decimalAdded = false;
+    zeroZeroAdded = false;
+    document.querySelector('.userInput').innerHTML = 0;
+  }
+// end of registerLogic function  
+})()
